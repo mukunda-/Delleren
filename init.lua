7c811b2c978af8e4a5542257e0e958237a1302d2
mukunda- <mukunda@mukunda.com>
2015-10-02 09:43:15 -0500

    Version bump.

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@
 -- See LICENSE-DELLEREN.TXT
 -------------------------------------------------------------------------------
 
-local VERSION = "1.2.0"
+local VERSION = "1.2.1"
 
 -------------------------------------------------------------------------------
 DellerenAddon = LibStub("AceAddon-3.0"):NewAddon( "Delleren", 
